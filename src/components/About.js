24641144e05bbb7bac3da4c0c057988b536f033f
joengatia <ngatiajoe2@gmail.com>
2024-08-07 21:08:15 +0300

    Added new feautres as labs

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { image } from "../data/data";
+import { image } from "../data/data"; // Import the image variable
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>Welcome to my portfolio page. Here you can learn more about me and my work.</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,9 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
+
 function App() {
+
   return (
     <div>
       <NavBar />
